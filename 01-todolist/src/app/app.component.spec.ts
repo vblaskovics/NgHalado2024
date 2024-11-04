e3993877c684a483ad67b276d5cbe15f1bedd11a
vblaskovics <viktor.blaskovics@gmail.com>
2024-11-04 13:46:33 +0100

    [01] add test: should have a title

--- 01-todolist/src/app/app.component.spec.ts
+++ 01-todolist/src/app/app.component.spec.ts
@@ -1,35 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+
+let fixture: ComponentFixture<AppComponent>;
+let component: AppComponent;
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterModule.forRoot([])],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '01-todolist'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('01-todolist');
+  it('should have a title', () => {
+    const title = fixture.debugElement.query(By.css('h1'));
+    expect(title.nativeElement.textContent).toEqual('Feladat lista');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 01-todolist');
-  });
+  
 });
