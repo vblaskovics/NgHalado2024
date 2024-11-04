59dd5ad552ae659d4ee9103ef02991b5eb055601
vblaskovics <viktor.blaskovics@gmail.com>
2024-11-04 14:53:21 +0100

    [01] add dialog + test

--- 01-todolist/src/app/app.component.spec.ts
+++ 01-todolist/src/app/app.component.spec.ts
@@ -2,9 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 let fixture: ComponentFixture<AppComponent>;
 let component: AppComponent;
+
+let newBtn: DebugElement;
+let deleteBtn: DebugElement;
+let openList: DebugElement;
+let doneList: DebugElement;
+let dialog: DebugElement;
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -17,6 +24,9 @@ describe('AppComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+
+    newBtn = fixture.debugElement.query(By.css('[data-testid="new-btn"]'));
+    dialog = fixture.debugElement.query(By.css('dialog'));
   });
 
   it('should create the app', () => {
@@ -29,5 +39,21 @@ describe('AppComponent', () => {
     expect(title.nativeElement.textContent).toEqual('Feladat lista');
   });
 
-  
+  it('should render "Új feladat" button', () => {
+    expect(newBtn).toBeTruthy();
+  })
+
+  it('should not show "Új feladat létrehozása" dialog initially', () => {
+    expect(dialog).withContext('Dialog should exist').toBeTruthy();
+    expect('open' in dialog.attributes).toBeFalse();
+  })
+
+  it('should show "Új feladat létrehozása" dialog when "Új feladat" button has been clicked', () => {
+    newBtn.triggerEventHandler('click');
+    fixture.detectChanges();
+    expect('open' in dialog.attributes).withContext('Dialog component should have the "open" attribute').toBeTrue();
+  })
+
+
+
 });
--- 01-todolist/src/app/app.component.ts
+++ 01-todolist/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  public isNewDialogOpen: boolean;
+
+  constructor() {
+    this.isNewDialogOpen = false;
+  }
+
+  onClickNew():void {
+    this.isNewDialogOpen = true;
+  }
 }
